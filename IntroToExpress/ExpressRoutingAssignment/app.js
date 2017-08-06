e94daf32dbe3f63d26c0a113583500faae44b1f4
Jeffrey McCrea <mccrea.jeffrey@gmail.com>
2017-08-06 21:40:43 +0000

    routing assignment initial commit

---
+++ IntroToExpress/ExpressRoutingAssignment/app.js
@@ -0,0 +1,19 @@
+/*
+1. create a brand new express app from scratch
+2. create a package.json using 'npm init' and add express as a dependency
+3. in your main app.js file, add 3 different routes:
+
+visiting "/" should print "Hi there, wlecome to my assignment!"
+
+visiting '/speak/pig' shoul dprint 'the pig says "Oink"'
+visiting '/speak/cow' shoul dprint 'the cow says "Moo"'
+visiting '/speak/dog' shoul dprint 'the dog says "Woof Woof!"'
+===============================================================
+visiting '/repeat/hello/3' shoul dprint 'the pig says "hello hello hello"'
+visiting '/repeat/hello/5' shoul dprint 'the pig says "hello hello hello hello hello"'
+visiting '/repeat/blah/2' shoul dprint 'the pig says "blah blah"'
+
+
+any other routes should return/print
+"sorry, page not found..what are you doing with your life?"
+*/
