607034892b256289655e74e8139878c6b6e71bcb
Jeffrey McCrea <mccrea.jeffrey@gmail.com>
2017-08-06 20:06:22 +0000

    end of express domo

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,1 +1,26 @@
 console.log("Our Express App will go Here");
+
+var express = require('express');
+var app = express();
+
+// '/' => "hi there!"
+app.get("/", function(req, res){
+   res.send('Hi there!');
+});
+
+
+// "/bye" => "goodbye!"
+app.get('/bye', function(req, res){
+    res.send('goodbye!');
+})
+
+// "/dog" => "Meow!"
+app.get('/dog', function(req, res){
+    console.log('someone made a request to /dog!');
+    res.send("Woof!");
+})
+
+// Tell express to listen for requests (start server)
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('server has started!');
+});
