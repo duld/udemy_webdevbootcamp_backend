fc461765cd2ee8306824667777d467d14ebc5ad6
Jeffrey McCrea <mccrea.jeffrey@gmail.com>
2017-08-06 21:58:23 +0000

    solution to basic routing assignment

--- IntroToExpress/ExpressRoutingAssignment/app.js
+++ IntroToExpress/ExpressRoutingAssignment/app.js
@@ -17,3 +17,51 @@ visiting '/repeat/blah/2' shoul dprint 'the pig says "blah blah"'
 any other routes should return/print
 "sorry, page not found..what are you doing with your life?"
 */
+
+var express = require('express');
+
+var app = express();
+
+// root
+app.get('/', function(req, res){
+    res.send("Hi there, wlecome to my assignment!");
+});
+
+// animal noises //
+// pig //
+app.get('/pig', function(req, res){
+    res.send("<h1>Oink!</h1>");
+});
+
+// cow //
+app.get('/cow', function(req, res){
+    res.send("<h1>Moo</h1>!");
+});
+
+// dog //
+app.get('/dog', function(req, res){
+    res.send("<h1>Woof Woof!</h1>");
+});
+
+// repeat strings // 
+app.get('/repeat/:saying/:count', function(req, res){
+    var out = "<h1>";
+    for (var i=0; i < req.params.count; i++){
+        out += req.params.saying + ' ';
+    }
+    res.send(out + "</h1>");
+})
+
+
+
+// catachall
+app.get('*', function(req, res){
+    res.send("Sorry, page not found...What are you doing with your life?");
+});
+
+
+
+// listen on port and ip
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server startup!");
+})
