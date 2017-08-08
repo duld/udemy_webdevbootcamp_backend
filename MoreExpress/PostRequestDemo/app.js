4451aaafa65d0486a4c4086fb0be9ce882482f5e
Jeffrey McCrea <mccrea.jeffrey@gmail.com>
2017-08-08 23:39:01 +0000

    Add bodyParser to allow for post body inspection

--- MoreExpress/PostRequestDemo/app.js
+++ MoreExpress/PostRequestDemo/app.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+var friends = ['Tony', 'Miranda', 'Justin', 'Pierre', "Lilly"];
+
 
 // GET requests
 app.get('/', function(req, res){
     res.render('home');
 });
 
-// POST requests
+app.get('/friends', function(req, res){
+    
+    
+    res.render('friends', {friends: friends});
+});
 
+// POST requests
+app.post('/addfriend', function(req, res){
+    var newFriend = (req.body.newfriend);
+    friends.push(newFriend);
+    res.redirect('/friends');
+});
 
 // set server to listen to port / ip
 app.listen(process.env.PORT, process.env.IP, function(){
