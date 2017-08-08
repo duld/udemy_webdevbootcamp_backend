e148a450d1ee51587ae877d3db566c232a1b4cb4
Jeffrey McCrea <mccrea.jeffrey@gmail.com>
2017-08-08 21:40:27 +0000

    passing variables to ejs template

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -13,6 +13,17 @@ app.get('/fallinlovewith/:thing', function(req, res){
     res.render('love.ejs', {thingVar: thing});
 });
 
+
+app.get('/posts', function(req, res){
+    var posts = [
+        { title: "Post1", author : "Susy"},
+        { title: 'One Tough Toad', author : 'Susy'},
+        { title: 'What is a pomsky?', author : 'Susy'}
+    ];
+    
+    res.render('posts.ejs', {posts: posts});
+})
+
 // start the app
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server is up running!");
